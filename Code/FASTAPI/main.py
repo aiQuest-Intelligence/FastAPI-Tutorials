11c3e426b0b0e58492541aaa66065ecbb6d2dc1b
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2025-10-13 15:26:45 +0600

    POST Method

--- Code/FASTAPI/main.py
+++ Code/FASTAPI/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
 
 
 app = FastAPI()
@@ -11,9 +15,25 @@ class Course(BaseModel):
     duration: float
     website: HttpUrl
 
+
+while True:
+    try:
+        conn = psycopg2.connect(host = "localhost", database = "aiquest", user = "postgres", password="1234", cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Successfully connected Datbase')
+        break
+
+    except Exception as error:
+        print('Database connection failed')
+        print("Error:",error)
+        time.sleep(2)
+
 @app.post("/post")
 def create_post(post: Course):
-    return{"data": post}
+    cursor.execute("""INSERT INTO course(name, instructor, duration, website) VALUES (%s,%s,%s,%s) RETURNING *""", (post.name, post.instructor, post.duration, str(post.website)))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return{"data": new_post}
 
 
 @app.get("/course")
@@ -22,7 +42,9 @@ def studymart():
 
 @app.get("/")
 def aiquest():
-    return {"Django":"API"}
+    cursor.execute(""" SELECT * FROM course """)
+    data = cursor.fetchall()
+    return {"Data":data}
 
 @app.get("/django/api")
 def django():
