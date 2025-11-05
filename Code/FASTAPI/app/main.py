7819ae9387c290c3ccc9154a9b178839bd270c63
Md. Abu Noman Basar <55510716+nomanbasar@users.noreply.github.com>
2025-11-05 14:17:46 +0600

    schemas.py

--- Code/FASTAPI/app/main.py
+++ Code/FASTAPI/app/main.py
@@ -3,9 +3,10 @@ from pydantic import BaseModel, HttpUrl
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from . import models
+from . import models, schemas
 from sqlalchemy.orm import Session
 from . database import engine, get_db
+from typing import List
 
 
 
@@ -13,12 +14,7 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-#define request body schema
-class Course(BaseModel):
-    name: str
-    instructor: str
-    duration: float
-    website: HttpUrl
+
 
 
 while True:
@@ -33,60 +29,56 @@ while True:
         print("Error:",error)
         time.sleep(2)
 
-@app.post("/post")
-def create_post(post: Course):
-    cursor.execute("""INSERT INTO course(name, instructor, duration, website) VALUES (%s,%s,%s,%s) RETURNING *""", (post.name, post.instructor, post.duration, str(post.website)))
-    new_post = cursor.fetchone()
-    conn.commit()
-    return{"data": new_post}
+# @app.post("/post")
+# def create_post(post: Course):
+#     cursor.execute("""INSERT INTO course(name, instructor, duration, website) VALUES (%s,%s,%s,%s) RETURNING *""", (post.name, post.instructor, post.duration, str(post.website)))
+#     new_post = cursor.fetchone()
+#     conn.commit()
+#     return{"data": new_post}
 
-@app.post('/courses')
-def create_course(course:Course, db: Session = Depends(get_db)):
-    new_course = models.Course(
-        name = course.name,
-        instructor = course.instructor,
-        duration = course.duration,
-        website = str(course.website)
-    )
+@app.post('/courses', response_model=schemas.CourseResponse)
+def create_course(course:schemas.CourseCreate, db: Session = Depends(get_db)):
+    new_course = models.Course(**course.model_dump())
+    new_course.website = str(course.website)
     db.add(new_course)
     db.commit()
     db.refresh(new_course)
-    return {"Course": new_course}
+    return new_course
 
 
-@app.get("/course")
-def studymart():
-    return {"Course": "Django & Backend API development with Python"}
+# @app.get("/course")
+# def studymart():
+#     return {"Course": "Django & Backend API development with Python"}
 
-@app.get("/")
-def aiquest():
-    cursor.execute(""" SELECT * FROM course """)
-    data = cursor.fetchall()
-    return {"Data":data}
+# @app.get("/")
+# def aiquest():
+#     cursor.execute(""" SELECT * FROM course """)
+#     data = cursor.fetchall()
+#     return {"Data":data}
 
-@app.get("/coursealchemy")
+@app.get("/coursealchemy", response_model= List[schemas.CourseResponse])
 def course(db:Session = Depends(get_db)):
-    course = db.query(models.Course).all()
-    return {"Course": course}
+    courses = db.query(models.Course).all()
+    return courses
 
-@app.get("/django/api")
-def django():
-    return {"Type": "Basict to Advanced"}
+# @app.get("/django/api")
+# def django():
+#     return {"Type": "Basict to Advanced"}
 
 
 
-@app.get("/course/{id}")
-def get_course(id:int):
-    cursor.execute(""" SELECT * FROM course WHERE id = %s """, (str(id),))
-    course = cursor.fetchone()
-    if not course:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail= f"Course with id:{id} was not found"
-        )
-    return{"Course_detail": course}
+# @app.get("/course/{id}")
+# def get_course(id:int):
+#     cursor.execute(""" SELECT * FROM course WHERE id = %s """, (str(id),))
+#     course = cursor.fetchone()
+#     if not course:
+#         raise HTTPException(
+#             status_code = status.HTTP_404_NOT_FOUND,
+#             detail= f"Course with id:{id} was not found"
+#         )
+#     return{"Course_detail": course}
 
-@app.get("/coursealchemy/{id}")
+@app.get("/coursealchemy/{id}", response_model=schemas.CourseResponse)
 def aiquest_course(id:int, db: Session = Depends(get_db)):
     course = db.query(models.Course).filter(models.Course.id == id).first()
     if not course:
@@ -94,17 +86,17 @@ def aiquest_course(id:int, db: Session = Depends(get_db)):
             status_code = status.HTTP_404_NOT_FOUND,
             detail= f"Course with id:{id} was not found"
         )
-    return{"Course_detail": course}
+    return course
 
 
-@app.delete("/course/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_course(id: int):
-    cursor.execute("""DELETE FROM course WHERE id = %s returning * """, ((str(id),)))
-    deleted_course = cursor.fetchone()
-    conn.commit()
-    if deleted_course == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"course with id: {id} does not exist")
-    return Response(status_code=status.HTTP_404_NOT_FOUND)
+# @app.delete("/course/{id}", status_code=status.HTTP_204_NO_CONTENT)
+# def delete_course(id: int):
+#     cursor.execute("""DELETE FROM course WHERE id = %s returning * """, ((str(id),)))
+#     deleted_course = cursor.fetchone()
+#     conn.commit()
+#     if deleted_course == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"course with id: {id} does not exist")
+#     return Response(status_code=status.HTTP_404_NOT_FOUND)
 
 @app.delete("/aiquest_course_delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_aiquest_course(id:int, db:Session=Depends(get_db)):
@@ -118,18 +110,18 @@ def delete_aiquest_course(id:int, db:Session=Depends(get_db)):
 
 
 
-@app.put("/course/{id}")
-def update_course(id: int, course: Course):
-    cursor.execute("""UPDATE course SET name = %s, instructor = %s, duration = %s, website = %s WHERE id = %s RETURNING * """, (course.name, course.instructor, course.duration, str(course.website), str(id)))
-    updated_course = cursor.fetchone()
-    conn.commit()
+# @app.put("/course/{id}")
+# def update_course(id: int, course: Course):
+#     cursor.execute("""UPDATE course SET name = %s, instructor = %s, duration = %s, website = %s WHERE id = %s RETURNING * """, (course.name, course.instructor, course.duration, str(course.website), str(id)))
+#     updated_course = cursor.fetchone()
+#     conn.commit()
 
-    if updated_course == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"course with id: {id} does not exist")
-    return{"data": updated_course}
+#     if updated_course == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"course with id: {id} does not exist")
+#     return{"data": updated_course}
 
-@app.put("/aiquest_course/{id}")
-def update_aiquest_course(id:int, updated_course: Course, db:Session=Depends(get_db)):
+@app.put("/aiquest_course/{id}", response_model=schemas.CourseResponse)
+def update_aiquest_course(id:int, updated_course: schemas.CourseCreate, db:Session=Depends(get_db)):
     course_query = db.query(models.Course).filter(models.Course.id==id)
     course = course_query.first()
     if not course:
@@ -139,12 +131,13 @@ def update_aiquest_course(id:int, updated_course: Course, db:Session=Depends(get
     course_query.update(update_data, synchronize_session=False)
     db.commit()
     db.refresh(course)
-    return {"Course_detail": course}
+    return course
 
 
 
 
     
   
+
 
 
